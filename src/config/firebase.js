fd4a18f44c359bb60d33aa287fcc5c62f0576faa
kizmo04 <get.kizmo@gmail.com>
2018-10-31 22:21:33 +0900

    edit gitignore, first deploy test

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+};
+
+export default firebase.initializeApp(config);
